dddb8bf27245505b5995ea5e665d616aefd0cbce
francisco jose <fjarquellada@vectoritcgroup.com>
2017-11-05 21:52:17 +0100

    Enlazando endpoints con los repositorios

--- src/main/java/es/fjarquellada/learningA2/linkedinlearningfullstackspringbootangular/rest/ReservationResource.java
+++ src/main/java/es/fjarquellada/learningA2/linkedinlearningfullstackspringbootangular/rest/ReservationResource.java
@@ -10,6 +10,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import es.fjarquellada.learningA2.linkedinlearningfullstackspringbootangular.rep
 
 @RestController
 @RequestMapping(value = ResourceConstants.ROOM_RESERVATION_V1)
+@CrossOrigin
 public class ReservationResource {
 
 	@Autowired
--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Component({
   selector: 'app-root',
@@ -8,27 +12,64 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
 
-
+  private BASE_URL = 'http://localhost:8080';
   public submitted: boolean;
   roomsearch: FormGroup;
   rooms: Room[];
-  constructor() { }
+  request: ReserveRoomRequest;
+  currentCheckInVal: string;
+  currentCheckOutVal: string;
+
+  constructor(private http: Http) { }
 
   ngOnInit(): void {
     this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
-
-    this.rooms = ROOMS;
+    const roomsearchValueChanges$ = this.roomsearch.valueChanges;
+    roomsearchValueChanges$.subscribe(
+      valueChange => {
+        this.currentCheckInVal = valueChange.checkin;
+        this.currentCheckOutVal = valueChange.checkout;
+      }
+    );
   }
 
   onSubmit = ({ value, valid }: { value: Roomsearch, valid: boolean }) => {
-    console.log('AppComponent :: onSubmit :: value ', value); 
+    console.log('AppComponent :: onSubmit :: value ', value);
+    this.getAll().subscribe(
+      rooms => {
+        this.rooms = rooms;
+      },
+      err => {
+        console.log("AppComponent :: onSubmit :: error: ", err);
+      }
+    );
   }
 
-  reserveRoom(value: string){
+  reserveRoom(value: string) {
     console.log("AppComponent :: reserveRoom :: value: ", value);
+    this.request = new ReserveRoomRequest(value, this.currentCheckInVal, this.currentCheckOutVal);
+
+  }
+
+  getAll(): Observable<Room[]> {
+    return this.http.get(this.BASE_URL + "/room/reservation/v1?checkin=" + this.currentCheckInVal + "&checkout="+this.currentCheckOutVal)
+      .map(this.mapRoom);
+  }
+
+  createReservation(body:ReserveRoomRequest){
+    let bodyString = JSON.stringify(body);
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let option = new RequestOptions({headers: headers});
+
+    this.http.post(this.BASE_URL + '/room/reservation/v1', body, option)
+    .subscribe(res => console.log("res: ", res))
+  }
+
+  mapRoom = (response: Response) => {
+    return response.json().content;
   }
 
 }
@@ -41,27 +82,18 @@ export interface Roomsearch {
 export interface Room {
   id: string;
   roomNumber: string;
-  price:string;
-  links:string;
+  price: string;
+  links: string;
 }
 
-var ROOMS:Room[] = [
-  {
-    "id" :        "38932123",
-    "roomNumber": "409",
-    "price":      "20",
-    "links":      ""
-  },
-  {
-    "id" :        "38932124",
-    "roomNumber": "410",
-    "price":      "22",
-    "links":      ""
-  },
-  {
-    "id" :        "38932125",
-    "roomNumber": "411",
-    "price":      "23",
-    "links":      ""
+export class ReserveRoomRequest {
+  roomId: string;
+  checkin: string;
+  checkout: string;
+
+  constructor(roomId: string, checkin: string, checkout: string) {
+    this.roomId = roomId;
+    this.checkin = checkin;
+    this.checkout = checkout;
   }
-]
+}
